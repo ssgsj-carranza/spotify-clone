df9f91fd1b25313c8fd23e9ce2b85561d80cdb78
unknown <ssgsj.carranza@gmail.com>
2021-06-09 14:41:54 -0500

    fixed bug where succesful login wouldnt redirect to home apge but back to sign in again even though a token was obtained

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import Login from './components/Login/Login';
+import React, {useEffect, useState} from 'react';
+import { getTokenFromUrl } from './spotify';
+
 
 function App() {
+  const [token, setToken] = useState(null);
+  //Run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = '';
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token)
+    }
+    console.log("token is here", token)
+  },[]);
   return (
-    <div className="app">
-      <Login />
+    <div className="app">{token ? (<h1>Logged in</h1>) : (
+        <Login />
+      )}
     </div>
   );
 }
