8a00f61c6cec8a22d5c012fa94580105bd310aca
unknown <ssgsj.carranza@gmail.com>
2021-06-10 20:31:31 -0500

    created reducer file, passed it to data layer render in index

--- src/index.js
+++ src/index.js
@@ -12,10 +12,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {DataLayer} from './DataLayer';
+import reducer, {initialState} from './reducer';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode inititalState={initialState} reducer={reducer}>
+    <DataLayer>
+      <App />
+    </DataLayer>
   </React.StrictMode>,
   document.getElementById('root')
 );
