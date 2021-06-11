e60947ae96620aea18ae4bc7fbf9b488fe67edf7
unknown <ssgsj.carranza@gmail.com>
2021-06-10 20:21:49 -0500

    created state provider file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 import Login from './components/Login/Login';
 import React, {useEffect, useState} from 'react';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './components/Player/Player';
 
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -13,13 +17,16 @@ function App() {
     const _token = hash.access_token;
     if (_token) {
       setToken(_token)
+      spotify.setAccessToken(_token);
+      spotify.getMe().then(user => {
+        console.log('user', user)
+      })
     }
     console.log("token is here", token)
   },[]);
   return (
-    <div className="app">{token ? (<h1>Logged in</h1>) : (
-        <Login />
-      )}
+    <div className="app">
+      {token ? <Player/> :<Login />}
     </div>
   );
 }
